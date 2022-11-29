e7a18586a300217861b4ee0ce085fda996ad5765
Richard Ragusski <richard_on@icloud.com>
2022-11-30 01:31:50 +0300

    Add more logging in init

--- config/init.go
+++ config/init.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"github.com/rs/zerolog"
 	"os"
 	"strconv"
@@ -27,38 +28,38 @@ func Init() error {
 
 	GoDotEnv, err = strconv.ParseBool(os.Getenv("GODOTENV"))
 	if err != nil {
-		return err
+		return fmt.Errorf("GODOTENV init: %v", err)
 	}
 
 	Log = os.Getenv("LOG")
 
 	LogCW, err = strconv.ParseBool(os.Getenv("LOG_CW"))
 	if err != nil {
-		return err
+		return fmt.Errorf("LOG_CW init: %v", err)
 	}
 
 	LogFile = os.Getenv("LOG_FILE")
 
 	LogLevel, err = zerolog.ParseLevel(os.Getenv("LOG_LEVEL"))
 	if err != nil {
-		return err
+		return fmt.Errorf("LOG_LEVEL init: %v", err)
 	}
 
 	FiberPrefork, err = strconv.ParseBool(os.Getenv("FIBER_PREFORK"))
 	if err != nil {
-		return err
+		return fmt.Errorf("FIBER_PREFORK init: %v", err)
 	}
 
 	MaxCPU, err = strconv.Atoi(os.Getenv("MAX_CPU"))
 	if err != nil {
-		return err
+		return fmt.Errorf("MAX_CPU init: %v", err)
 	}
 
 	SentryDSN = os.Getenv("SENTRY_DSN")
 
 	SentryTSR, err = strconv.ParseFloat(os.Getenv("SENTRY_TSR"), 64)
 	if err != nil {
-		return err
+		return fmt.Errorf("SENTRY_TSR init: %v", err)
 	}
 
 	return nil
